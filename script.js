4f222210a26fa4328a463ad0f0909a417f4addc3
Anna Lundstrøm <annalundstrom@Annas-MacBook-Pro.local>
2019-01-03 15:40:52 +0100

    commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+function myFunction() {
+    document.querySelector(".container").classList.toggle("change");
+    //toggle betyder at når man klikker og animationen er på, så går animationen fra.. og hvis man klikker og animationen ikke er på, så sætter den igang.. //
+
 }
+
+document.querySelector(".container").addEventListener("click", myFunction);
